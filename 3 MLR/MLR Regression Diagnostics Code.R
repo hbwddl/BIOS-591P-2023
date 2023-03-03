0b121a3602005d9c80a85d85020a15bcea20f59f
Hannah Waddel <hannah.b.waddel@emory.edu>
2023-03-03 12:33:48 -0500

    Update MLR Regression Diagnostics Code.R

--- 3 MLR/MLR Regression Diagnostics Code.R
+++ 3 MLR/MLR Regression Diagnostics Code.R
@@ -107,3 +107,36 @@ cor(bwt)
 ## VIFs
 vif(bwt.lm)
 
+## Replicate SAS fit diagnostics plots
+# par(mfrow=c(3,3)) # This sets the parameter for plots to a grid with 3 rows and 3 columns, if you want all plots in the same image
+# Predicted vs. residuals
+plot(predict(bwt.lm),residuals(bwt.lm),pch=19,xlab="Predicted Value",ylab="Residual")
+abline(h=0,col="blue") # Horizontal line at 0
+
+# Predicted vs. Studentized residuals
+plot(predict(bwt.lm),jackres,pch=19,xlab="Predicted Value",ylab="RStudent")
+abline(h=c(-2,2),col="blue") #Recall that c(-2,2) creates a list. Place horizontal lines at -2 and 2
+
+# Leverage vs. Studentized residuals
+plot(lev.vals,jackres,pch=19,xlab="Leverage",ylab="RStudent")
+abline(h=c(-2,2),col="blue")
+abline(v=lev.threshold,col="blue") # Vertical line at the leverage threshold
+
+# Q-Q Plot
+qqnorm(residuals(bwt.lm),pch=19)
+qqline(residuals(bwt.lm),col="blue")
+
+# Predicted vs. Actual values
+plot(predict(bwt.lm),bwt$BWT,pch=19,xlab="Predicted Values",ylab="Birthweight")
+abline(a=0,b=1,col="blue") # Intercept = 0, slope = 1. If a point is on the line, then the predicted value is close to the actual value
+
+# Cook's distance
+plot(cooksd,xlab="Observation",ylab="Cook's D", pch=19)
+abline(h=4/length(cooksd),col="blue") #This plot is harder to interpret for a lot of observations...
+
+# Histogram of residuals
+hist(residuals(bwt.lm),xlab="Residuals",probability=T,ylim=c(0,6e-04)) # Probability = T gives us percent instead of frequency. We set the ylim so that we can see the whole density line.
+curve(dnorm(x, mean=mean(residuals(bwt.lm)), sd=sd(residuals(bwt.lm))), 
+      col="blue", lwd=2, add=TRUE) # Adding a normal density with the same mean and standard deviation as the residuals
+
+# par(mfrow=c(1,1)) # Set the plot area to have only one plot again
